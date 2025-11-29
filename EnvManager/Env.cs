8264e333dcb1d1afe67ac1379a0f5b29109502b0
Meb <meb@caramail.com>
2025-11-29 15:42:00 +0100

    Switching from .NET 8.0 -> .NET Framework 4.8.1 for compatibility purpose

--- EnvManager/Env.cs
+++ EnvManager/Env.cs
@@ -1,21 +1,23 @@
-﻿using System.Text.Json;
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace EnvManager;
+namespace EnvManager {
+    public class Env {
+        internal string Uuid = "global";
+        public string Name { get; set; }
+        private static readonly JsonSerializerOptions JsonOptions = new JsonSerializerOptions { WriteIndented = true, IncludeFields = true };
+    
+        [JsonInclude]
+        private Dictionary<string, JsonElement> Attributes { get; set; } = new Dictionary<string, JsonElement>();
+    
+        public void SetAttribute(string key, object value) {
+            if (key == "") throw new ArgumentNullException(nameof(key));
+            Attributes[key] = JsonSerializer.SerializeToElement(value, JsonOptions);
+            FileManager.SaveEnv(this);
+        }
 
-public class Env {
-    internal string Uuid = "global";
-    public string? Name { get; set; }
-    private static readonly JsonSerializerOptions JsonOptions = new() { WriteIndented = true, IncludeFields = true };
-    
-    [JsonInclude]
-    private Dictionary<string, JsonElement> Attributes { get; set; } = new();
-    
-    public void SetAttribute(string key, object value) {
-        if (key == "") throw new ArgumentNullException(nameof(key));
-        Attributes[key] = JsonSerializer.SerializeToElement(value, JsonOptions);
-        FileManager.SaveEnv(this);
+        public T GetAttribute<T>(string key) => key == "" ? throw new ArgumentNullException(nameof(key)) : Attributes[key].Deserialize<T>(JsonOptions);
     }
-
-    public T? GetAttribute<T>(string key) => (key == "" ? throw new ArgumentNullException(nameof(key)) : Attributes[key].Deserialize<T>(JsonOptions))!;
 }
