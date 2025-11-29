ef6b2e8ffa37cc4f196bc8719c7ec731122e8c87
Meb <meb@caramail.com>
2025-11-29 10:15:44 +0100

    Init

---
+++ EnvManager/Env.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace EnvManager;
+
+public class Env {
+    internal string Uuid = "global";
+    public string? Name { get; set; }
+    
+    [JsonInclude]
+    private Dictionary<string, string> Attributes { get; set; } = new();
+    
+    public void SetAttribute(string key, object value)
+    {
+        if (key == "") throw new ArgumentNullException(nameof(key));
+        Attributes[key] = JsonSerializer.Serialize(value);
+        Console.WriteLine($"{key}: {JsonSerializer.Serialize(value)}");
+        FileManager.SaveEnv(this);
+    }
+
+    public T GetAttribute<T>(string key) {
+        return (key == "" ? throw new ArgumentNullException(nameof(key)) : JsonSerializer.Deserialize<T>(Attributes[key]))!;
+    }
+}
---
+++ EnvManager/EnvManager.cs
@@ -0,0 +1,13 @@
+﻿namespace EnvManager;
+
+public static class EnvManager {
+    public static Env GetEnv(string uuid="global", string name="") {
+        if (uuid == "global")
+            name = "Global Attributes";
+        Env env = FileManager.GetEnv(uuid);
+        if (name == "" || name == env.Name) return env;
+        env.Name = name;
+        FileManager.SaveEnv(env);
+        return env;
+    }
+}
